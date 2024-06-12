3afb69a6ae7abe1b3b61c99e8c62eb8c9a02f4da
Yoan Dusoleil <y.dusoleil@outlook.com>
2024-06-12 17:59:10 +0200

    feat 🖥️: init webui

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button.tsx";
+
+function App() {
+    return (
+        <>
+            <p className={"text-4xl"}>hihi</p>
+            <Button>Click me</Button>
+        </>
+    );
+}
+
+export default App;
